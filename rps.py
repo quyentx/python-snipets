582870e2b1b07f25fc96103238bec29718b536b6
Quyen Ta <quyentx@users.noreply.github.com>
2020-03-06 09:08:57 +0700

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,18 @@
+import random
+options = ["rock", "paper", "scissors"]
+player1 = random.choice(options)
+print("The computer made its choice. It's your turn!")
+player2 = input("Your choice: ")
+if player2 in options:
+  if player1 == player2:
+      print("Draw")
+  elif player1 == "rock" and player2 == "scissors":
+      print("Computer plays "+player1+". Computer wins!")
+  elif player1 == "paper" and player2 == "rock":
+      print("Computer plays "+player1+". Computer wins!")
+  elif player1 == "scissors" and player2 == "paper":
+      print("Computer plays "+player1+". Computer wins!")
+  else:
+      print("Computer plays "+player1+". You wins!")
+else:
+  print("***No cheating***")
